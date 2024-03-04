6fafc9183420e80707add0920456852d6175c4dd
deveshsen <deveshsen512@gmail.com>
2024-03-05 00:33:10 +0530

    day 5 update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
-import "package:flutter_catalog/pages/home.dart";
 import "package:flutter_catalog/pages/login.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,14 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String dev = "world best programmer";
+    // String dev = "world best programmer";
 
     // final pi = 3.2;
     // const devesh = "devesh";
     // var deve = 1;
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        // primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         "/": (context) => Login(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Login({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 50,
-              color: Colors.pink,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic),
-        ),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("Welcome User"),
+          TextField(
+            controller: usernameController,
+            decoration: const InputDecoration(
+              labelText: 'Username',
+              hintText: "Enter Your Username",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: "Enter Your Password",
+              labelText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 32),
+          ElevatedButton(
+            onPressed: () {
+              // Add your login logic here
+              print('Username: ${usernameController.text}');
+              print('Password: ${passwordController.text}');
+            },
+            child: const Text(    
+              'Login',
+              style: TextStyle(fontSize: 10),
+            ),
+          ),
+        ],
       ),
     );
   }

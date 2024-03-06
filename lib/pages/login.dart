3742a5bf9491d84814f31c55686ff6c2b0bfc657
deveshsen <deveshsen512@gmail.com>
2024-03-06 16:12:50 +0530

    day 8

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,112 +17,134 @@ class _LoginState extends State<Login> {
   bool changeButton = false;
 
   final _formKey = GlobalKey<FormState>();
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState?.validate() ?? false) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRotes);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/login.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Welcome $name",
-              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: usernameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Username',
-                      hintText: "Enter Your Username",
-                      border: OutlineInputBorder(),
-                    ),
-                    onChanged: (value) {
-                      name = value;
-
-                      setState(() {});
-                      //this setState change the UI and callback the build method
-                    },
-                  ),
-                  SizedBox(height: 16),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "Enter Your Password",
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 28),
-                  InkWell(
-                    onTap: () async {
-                      setState(() {
-                        changeButton = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      await Navigator.pushNamed(context, MyRoutes.homeRotes);
-                      setState(() {
-                        changeButton = false;
-                      });
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      width: changeButton ? 50 : 150,
-                      height: 50,
-                      alignment: Alignment.center,
-                      child: changeButton
-                          ? Icon(
-                              Icons.done,
-                              color: Color.fromARGB(255, 185, 188, 204),
-                            )
-                          : Text(
-                              "login",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18),
-                            ),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 52, 52, 198),
-                          // shape:
-                          //     changeButton ? BoxShape.circle : BoxShape.rectangle,
-                          borderRadius:
-                              BorderRadius.circular(changeButton ? 50 : 8)),
-                    ),
-                  )
-
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     // Add your login logic here
-                  //     print('Username: ${usernameController.text}');
-                  //     print('Password: ${passwordController.text}');
-                  //     Navigator.pushNamed(context, MyRoutes.homeRotes);
-                  //   },
-                  //   child: const Text(
-                  //     'Login',
-                  //     style: TextStyle(
-                  //         fontSize: 18,
-                  //         fontWeight: FontWeight.w500,
-                  //         color: Colors.white),
-                  //   ),
-                  //   style: TextButton.styleFrom(
-                  //       minimumSize: Size(200, 50),
-                  //       backgroundColor: Color.fromARGB(255, 107, 81, 211)),
-                  // ),
-                ],
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/login.png",
+                fit: BoxFit.cover,
               ),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Welcome $name",
+                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        hintText: "Enter Your Username",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return "UserName Required";
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+
+                        setState(() {});
+                        //this setState change the UI and callback the build method
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Your Password",
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return "Password is required";
+                        } else if (value!.length < 6) {
+                          return "Password length should be atleast 6";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? Icon(
+                                  Icons.done,
+                                  color: Color.fromARGB(255, 185, 188, 204),
+                                )
+                              : Text(
+                                  "login",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                        ),
+                      ),
+                    )
+
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     // Add your login logic here
+                    //     print('Username: ${usernameController.text}');
+                    //     print('Password: ${passwordController.text}');
+                    //     Navigator.pushNamed(context, MyRoutes.homeRotes);
+                    //   },
+                    //   child: const Text(
+                    //     'Login',
+                    //     style: TextStyle(
+                    //         fontSize: 18,
+                    //         fontWeight: FontWeight.w500,
+                    //         color: Colors.white),
+                    //   ),
+                    //   style: TextButton.styleFrom(
+                    //       minimumSize: Size(200, 50),
+                    //       backgroundColor: Color.fromARGB(255, 107, 81, 211)),
+                    // ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+  final img =
+      "https://media.licdn.com/dms/image/D4D03AQEpIOpeiyHYOw/profile-displayphoto-shrink_800_800/0/1695630420571?e=2147483647&v=beta&t=nHR8ViZmImLfiwHSzVtObUY7AhnPhXf6psxNsIIopfo";
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                    accountName: Text("Devesh"),
+                    accountEmail: Text("deveshsen462046"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(img),
+                    ))),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.settings,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Setting",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.delete_left,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Logout",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
